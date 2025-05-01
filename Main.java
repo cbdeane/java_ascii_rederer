164a47af9ab9cffd32dafdf66e5a702ffaa1f89e
Charles Deane <30459123+cbdeane@users.noreply.github.com>
2025-04-30 20:57:21 -0400

    renamed the variable that used to be named shrek

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		MyImage shrek = new MyImage("fish.png");
-		System.out.print(shrek.getAscii());
+		MyImage mainImage = new MyImage("monalisa.jpg");
+		System.out.print(mainImage.getAscii());
 	}
 }
-
