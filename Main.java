ff692a74d93c3756c763a9c844e4942b0fe2b4a2
Charles Deane <30459123+cbdeane@users.noreply.github.com>
2025-05-02 20:53:59 -0400

    now file agnostic

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		MyImage mainImage = new MyImage("monalisa.jpg");
+		MyImage mainImage = new MyImage("");
 		System.out.print(mainImage.getAscii());
 	}
 }
